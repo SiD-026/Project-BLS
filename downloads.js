49f84df5616b8d2b873e6792399ae7c2204d6445
SiD <sidysid26@gmail.com>
2020-07-11 00:49:34 +0530

    beta-version-of-frontend

---
+++ downloads.js
@@ -0,0 +1,10 @@
+var menu = document.getElementById('menu')
+var nav = document.getElementById('nav')
+var exit = document.getElementById('exit')
+console.log(nav)
+menu.addEventListener('click' , () => {
+   nav.classList.toggle('navcon')
+})
+exit.addEventListener('click' , () => {
+    nav.classList.toggle('navcon')
+ })
